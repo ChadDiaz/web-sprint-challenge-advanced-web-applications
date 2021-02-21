211ff85177cf2d3209033f4f83ca4deb0649f5c8
ChadDiaz <chadcdiaz@gmail.com>
2021-02-21 17:07:26 -0500

    testing isnt working

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -1,13 +1,22 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import BubblePage from "./BubblePage";
+import Color from './Color'
+import { fetchColors as mockFetchColors } from '../helpers/fetchColors';
+import {res} from '../helpers/res'
+
+jest.mock('../helpers/fetchColors')
 
 test("Renders BubblePage without errors", () => {
   // Finish this test
+  render(<BubblePage/>)
 });
 
-test("Fetches data and renders the bubbles on mounting", () => {
+test("Fetches data and renders the bubbles on mounting", async () => {
   // Finish this test
+  mockFetchColors.mockResolvedValueOnce(res)
+  render(<BubblePage />)
+  await waitFor(() => expect(screen.getAllByTestId(/color/i)).toHaveLength(3));
 });
 
 //Task List
